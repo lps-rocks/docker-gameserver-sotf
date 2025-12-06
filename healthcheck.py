79b123ccb8bd41db48d75c916c31d21ece33e6c9
lps-rocks <10893911+lps-rocks@users.noreply.github.com>
2025-12-06 16:09:19 -0500

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -2,7 +2,7 @@
 import a2s
 import os
 
-ADDRESS = ("127.0.0.1", os.getenv("QUERY_PORT", 27016))  # Update port if needed
+ADDRESS = ("127.0.0.1", int(os.getenv("QUERY_PORT", 27016)))  # Update port if needed
 
 try:
     info = a2s.info(ADDRESS, timeout=3)
